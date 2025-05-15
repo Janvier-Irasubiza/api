063b1be70679f8d7cebe37715809da64b38a8af4
Janvier-Irasubiza <61458240+Janvier-Irasubiza@users.noreply.github.com>
2025-05-15 16:42:49 +0200

    Update ALLOWED_HOSTS and CORS settings for production environment

--- api/settings.py
+++ api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_v(=(2vb314sc^wg^#ej@ktm#9l(#1($jh_zh09v5pm+vghka6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "backend.healthylifeinitiative.com"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "bd.urugowoc.bscholarz.com"]
 
 # Application definition
 
@@ -65,13 +65,13 @@ SIMPLE_JWT = {
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "https://urugowoc.healthylifeinitiative.com",
+    "https://urugowoc.bscholarz.com",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://urugowoc.healthylifeinitiative.com",
+    "https://urugowoc.bscholarz.com",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
